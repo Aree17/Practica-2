e8c6f65fd3c4874a5bd1a8ed78d6fb813e76eed7
Arelys Ajila <arelys.ajila@unl.edu.ec>
2025-06-08 08:57:40 -0500

    Método ordenación
    Se modifico daoCancion para poder comparar enteros como el del atributo "duracion"

--- unl-sing/src/main/java/unl/sing/base/controller/dao/dao_models/DaoCancion.java
+++ unl-sing/src/main/java/unl/sing/base/controller/dao/dao_models/DaoCancion.java
@@ -66,8 +66,8 @@ public class DaoCancion extends AdapterDao<Cancion> {
         }
     }
 
-    public LinkedList<HashMap<String, String>> all() throws Exception{
-        LinkedList<HashMap<String, String>> lista = new LinkedList<>();
+    public LinkedList<HashMap<String, Object>> all() throws Exception {
+        LinkedList<HashMap<String, Object>> lista = new LinkedList<>();
         if (!this.listAll().isEmpty()) {
             Cancion[] arreglo = this.listAll().toArray();
             for (int i = 0; i < arreglo.length; i++) {
@@ -77,83 +77,134 @@ public class DaoCancion extends AdapterDao<Cancion> {
         return lista;
     }
 
-    private HashMap<String, String> toDict(Cancion arreglo, Integer i) {
+    private HashMap<String, Object> toDict(Cancion arreglo, Integer i) {
         DaoGenero dg = new DaoGenero();
         DaoAlbum da = new DaoAlbum();
-        HashMap<String, String> aux = new HashMap<>();
+        HashMap<String, Object> aux = new HashMap<>();
         aux.put("id", arreglo.getId().toString());
         aux.put("nombre", arreglo.getNombre());
         aux.put("genero", dg.listAll().get(arreglo.getId_genero() - 1).getNombre());
         aux.put("album", da.listAll().get(arreglo.getId_album() - 1).getNombre());
-        aux.put("duracion", arreglo.getDuracion().toString());
+        aux.put("duracion", arreglo.getDuracion());
         aux.put("url", arreglo.getUrl());
         aux.put("tipo", arreglo.getTipo().toString());
         return aux;
     }
 
-    public LinkedList<HashMap<String, String>> orderByCancion(Integer type, String attribute)throws Exception{
-        LinkedList<HashMap<String, String>> lista = all();
-        if(!listAll().isEmpty()){
+    public LinkedList<HashMap<String, Object>> orderByCancion(Integer type, String attribute) throws Exception {
+        LinkedList<HashMap<String, Object>> lista = all();
+        if (!listAll().isEmpty()) {
             HashMap arr[] = lista.toArray();
-            quickSort(arr, 0, arr.length-1, type, attribute);
+            quickSort(arr, 0, arr.length - 1, type, attribute);
             lista.toList(arr);
         }
         return lista;
     }
 
-
-    /*public HashMap<String, Object> BinarySearchRecursive(HashMap<String, Object> arr[], int a, int b, String attribute){
-        if(b < 1){
-            return null;
-        }
-        int n = a + (b=1)/2;
-        if(arr[].ge)
-    }*/
-
-
+    /*
+     * public HashMap<String, Object> BinarySearchRecursive(HashMap<String, Object>
+     * arr[], int a, int b, String attribute){
+     * if(b < 1){
+     * return null;
+     * }
+     * int n = a + (b=1)/2;
+     * if(arr[].ge)
+     * }
+     */
 
     public void quickSort(HashMap arr[], int begin, int end, Integer type, String attribute) {
         if (begin < end) {
             int partitionIndex = partition(arr, begin, end, type, attribute);
-    
-            quickSort(arr, begin, partitionIndex-1, type, attribute);
-            quickSort(arr, partitionIndex+1, end, type, attribute);
+
+            quickSort(arr, begin, partitionIndex - 1, type, attribute);
+            quickSort(arr, partitionIndex + 1, end, type, attribute);
         }
     }
 
-    private int partition(HashMap arr[], int begin, int end, Integer type, String attribute) {
+    /*private int partition(HashMap arr[], int begin, int end, Integer type, String attribute) {
         HashMap pivot = arr[end];
-        int i = (begin-1);
-        if(type==Utiles.ASCENDENTE){
+        int i = (begin - 1);
+        if (type == Utiles.ASCENDENTE) {
+            if (attribute == "duracion") {
+                for (int j = begin; j < end; j++) {
+                    if (((Integer) arr[j].get(attribute)) < (Integer) pivot.get(attribute)) {
+                        i++;
+                        HashMap swapTemp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = swapTemp;
+                    }
+                }
+            } else {
+                for (int j = begin; j < end; j++) {
+                    if (arr[j].get(attribute).toString().toLowerCase()
+                            .compareTo(pivot.get(attribute).toString().toLowerCase()) < 0) {
+                        i++;
+
+                        HashMap swapTemp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = swapTemp;
+                    }
+                }
+            }
+        } else {
+            if (attribute == "duracion") {
+                for (int j = begin; j < end; j++) {
+                    if (((Integer) arr[j].get(attribute)) > (Integer) pivot.get(attribute)) {
+                        i++;
+                        HashMap swapTemp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = swapTemp;
+                    }
+                }
+            } else {
+                for (int j = begin; j < end; j++) {
+                    if (arr[j].get(attribute).toString().toLowerCase()
+                            .compareTo(pivot.get(attribute).toString().toLowerCase()) > 0) {
+                        i++;
+                        HashMap swapTemp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = swapTemp;
+                    }
+                }
+            }
+        }
+
+        HashMap swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
+    }*/
+
+    private int partition(HashMap<String, Object> arr[], int begin, int end, Integer type, String attribute) {
+        HashMap<String, Object> pivot = arr[end];
+        int i = (begin - 1);
+        if (type == Utiles.ASCENDENTE) {
             for (int j = begin; j < end; j++) {
-                if (arr[j].get(attribute).toString().toLowerCase()
-                .compareTo(arr[i].get(attribute).toString().toLowerCase()) < 0) {
+                if (arr[j].get(attribute).toString().compareTo(pivot.get(attribute).toString()) < 0) {
+                    // if (arr[j] <= pivot) {
                     i++;
-        
-                    HashMap swapTemp = arr[i];
+                    HashMap<String, Object> swapTemp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = swapTemp;
                 }
             }
-        }else{
+        } else {
             for (int j = begin; j < end; j++) {
-                if (arr[j].get(attribute).toString().toLowerCase()
-                .compareTo(arr[i].get(attribute).toString().toLowerCase()) > 0) {
+                if (arr[j].get(attribute).toString().compareTo(pivot.get(attribute).toString()) > 0) {
                     i++;
-        
-                    HashMap swapTemp = arr[i];
+                    HashMap<String, Object> swapTemp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = swapTemp;
                 }
             }
         }
-    
-        HashMap swapTemp = arr[i+1];
-        arr[i+1] = arr[end];
+        HashMap<String, Object> swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
         arr[end] = swapTemp;
-    
-        return i+1;
+
+        return i + 1;
     }
-     
+    
 
 }
